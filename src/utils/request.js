fb1a1b0208ed76eb94bd5ce098fb73654f06fcfd
风之忧伤 <575792372@qq.com>
2021-06-16 11:48:24 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
+import { getToken } from '@/utils/auth'
 
 // 设置axios跨域访问
 axios.defaults.withCredentials = true // 设置cross跨域 并设置访问权限 允许跨域携带cookie信息
@@ -8,7 +9,8 @@ axios.defaults.crossDomain = true // 设置axios跨域的配置
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // base api url
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
   timeout: 3 * 60 * 1000 // request timeout
 })
 
@@ -16,7 +18,13 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-    config.headers['X-Token'] = 123
+
+    if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      config.headers['X-Token'] = getToken()
+    }
     return config
   },
   error => {
@@ -28,8 +36,20 @@ service.interceptors.request.use(
 
 // response interceptor
 service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
   response => {
-    const res = response.data;
+    const res = response.data
+
+    // if the res is null, it is judged as an error.
     if (res) {
       return res
     } else {
